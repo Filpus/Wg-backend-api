31a0b3f8841e1743f48b23033d6fa541801d6b30
Filpus <fil-pus@wp.pl>
2025-05-07 21:39:19 +0200

    Demography View End Nodes

--- Wg-backend-api/Controllers/GameControllers/LocalisationsController.cs
+++ Wg-backend-api/Controllers/GameControllers/LocalisationsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Wg_backend_api.Data;
+using Wg_backend_api.DTO;
 using Wg_backend_api.Models;
 
 namespace Wg_backend_api.Controllers.GameControllers
@@ -26,18 +27,39 @@ namespace Wg_backend_api.Controllers.GameControllers
             _context = _gameDbContextFactory.Create(schema);
         }
 
-        // GET: api/Localisations
+        // GET: api/Localisations  
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Localisation>>> GetLocalisation()
+        public async Task<ActionResult<IEnumerable<LocalisationDTO>>> GetLocalisation()
         {
-            return await _context.Localisations.ToListAsync();
+            return await _context.Localisations
+                .Select(l => new LocalisationDTO
+                {
+                    Id = l.Id,
+                    Name = l.Name,
+                    NationId = l.NationId,
+                    Fortification = l.Fortification,
+                    Size = l.Size
+
+                })
+                .ToListAsync();
         }
 
-        // GET: api/Localisations/5
+        // GET: api/Localisations/5  
         [HttpGet("{id}")]
-        public async Task<ActionResult<Localisation>> GetLocalisation(int? id)
+        public async Task<ActionResult<LocalisationDTO>> GetLocalisation(int? id)
         {
-            var localisation = await _context.Localisations.FindAsync(id);
+            var localisation = await _context.Localisations
+                .Where(l => l.Id == id)
+                .Select(l => new LocalisationDTO
+                {
+                    Id = l.Id,
+                    Name = l.Name,
+                    NationId = l.NationId,
+                    Fortification = l.Fortification,
+                    Size = l.Size
+
+                })
+                .FirstOrDefaultAsync();
 
             if (localisation == null)
             {
@@ -47,15 +69,23 @@ namespace Wg_backend_api.Controllers.GameControllers
             return localisation;
         }
 
-        // PUT: api/Localisations/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        // PUT: api/Localisations/5  
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutLocalisation(int? id, Localisation localisation)
+        public async Task<IActionResult> PutLocalisation(int? id, LocalisationDTO localisationDto)
         {
-            if (id != localisation.Id)
+            if (id != localisationDto.Id)
             {
                 return BadRequest();
             }
+
+            var localisation = await _context.Localisations.FindAsync(id);
+            if (localisation == null)
+            {
+                return NotFound();
+            }
+
+            localisation.Name = localisationDto.Name;
+            localisation.NationId = localisationDto.NationId;
 
             _context.Entry(localisation).State = EntityState.Modified;
 
@@ -78,21 +108,27 @@ namespace Wg_backend_api.Controllers.GameControllers
             return NoContent();
         }
 
-        // POST: api/Localisations
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        // POST: api/Localisations  
         [HttpPost]
-        public async Task<ActionResult<Localisation>> PostLocalisation(Localisation localisation)
+        public async Task<ActionResult<LocalisationDTO>> PostLocalisation(LocalisationDTO localisationDto)
         {
+            var localisation = new Localisation
+            {
+                Name = localisationDto.Name,
+                NationId = localisationDto.NationId,
+                Fortification = localisationDto.Fortification,
+                Size = localisationDto.Size
+            };
 
-            localisation.Id = null;
             _context.Localisations.Add(localisation);
             await _context.SaveChangesAsync();
 
+            localisationDto.Id = localisation.Id;
 
-            return CreatedAtAction("GetLocalisation", new { id = localisation.Id }, localisation);
+            return CreatedAtAction("GetLocalisation", new { id = localisation.Id }, localisationDto);
         }
 
-        // DELETE: api/Localisations/5
+        // DELETE: api/Localisations/5  
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteLocalisation(int? id)
         {
@@ -111,6 +147,145 @@ namespace Wg_backend_api.Controllers.GameControllers
         private bool LocalisationExists(int? id)
         {
             return _context.Localisations.Any(e => e.Id == id);
+        }
+
+        [HttpGet("Nation/{nationId}/GeneralInfo")]
+        public async Task<ActionResult<IEnumerable<LocalisationGeneralInfoDTO>>> GetLocalisationsGeneralInfoByNation(int nationId)
+        {
+            var localisations = await _context.Localisations
+                .Where(l => l.NationId == nationId)
+                .Select(l => new LocalisationGeneralInfoDTO
+                {
+                    Name = l.Name,
+                    Size = l.Size,
+                    Fortification = l.Fortification,
+                    PopulationSize = _context.Populations.Where(p => p.Location.Id == l.Id).Count(),
+                    PopulationHappiness = _context.Populations.Where(p => p.Location.Id == l.Id).Average(p => p.Happiness)
+                })
+                .ToListAsync();
+
+            return Ok(localisations);
+        }
+
+        [HttpGet("{id}/Details")]
+        public async Task<ActionResult<LocalisationDetailsDTO>> GetLocalisationDetails(int id)
+        {
+            var localisation = await _context.Localisations
+                .Where(l => l.Id == id)
+                .Select(l => new LocalisationDetailsDTO
+                {
+                    Name = l.Name,
+                    Resources = GetLocalisationResources(id),
+                    ResourceProductions = GetLocalisationResourceProductions(id),
+                    PopulationGroups = GetPopulationGroups(id)
+                })
+                .FirstOrDefaultAsync();
+
+            if (localisation == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(localisation);
+        }
+
+        private List<LocalisationResourceInfoDTO> GetLocalisationResources(int localisationId)
+        {
+            var localisationList = _context.LocalisationResources
+               .Where(lr => lr.LocationId == localisationId)
+               .Select(lr => new LocalisationResourceInfoDTO
+               {
+                   Name = _context.Localisations
+                       .Where(l => l.Id == lr.LocationId)
+                       .Select(l => l.Name)
+                       .FirstOrDefault(),
+                   ResourceName = _context.Resources
+                       .Where(r => r.Id == lr.ResourceId)
+                       .Select(r => r.Name)
+                       .FirstOrDefault(),
+                   Amount = lr.Amount
+               })
+               .ToList();
+
+            if (localisationList == null)
+            {
+                return new List<LocalisationResourceInfoDTO>();
+            }
+            else
+            {
+                return localisationList;
+            }
+        }
+        private List<LocalisationResourceProductionDTO> GetLocalisationResourceProductions(int localisationId)
+        {
+            return _context.LocalisationResources
+               .Where(lr => lr.LocationId == localisationId)
+               .Select(lr => new LocalisationResourceProductionDTO
+               {
+                   ResourceName = _context.Resources
+                       .Where(r => r.Id == lr.ResourceId)
+                       .Select(r => r.Name)
+                       .FirstOrDefault(),
+                   Amount = CalculateProductionAmount(lr.ResourceId, localisationId)
+               })
+               .ToList();
+        }
+
+        private List<PopulationGroupDTO> GetPopulationGroups(int localisationId)
+        {
+            return _context.Populations
+                           .Where(p => p.LocationId == localisationId)
+                           .GroupBy(p => new { p.SocialGroupId, p.CultureId, p.ReligionId })
+                           .Select(g => new PopulationGroupDTO
+                           {
+                               SocialGroup = _context.SocialGroups
+                                   .Where(sg => sg.Id == g.Key.SocialGroupId)
+                                   .Select(sg => sg.BaseHappiness.ToString())
+                                   .FirstOrDefault(),
+                               Culture = _context.Cultures
+                                   .Where(c => c.Id == g.Key.CultureId)
+                                   .Select(c => c.Id.ToString())
+                                   .FirstOrDefault(),
+                               Religion = _context.Religions
+                                   .Where(r => r.Id == g.Key.ReligionId)
+                                   .Select(r => r.Id.ToString())
+                                   .FirstOrDefault(),
+                               Amount = g.Count(),
+                               Happiness = g.Average(p => p.Happiness)
+                           })
+                           .ToList();
+        }
+
+        private float CalculateProductionAmount(int resourceId, int localisationId)
+        {
+            var populationGroups = _context.Populations
+                .Where(p => p.LocationId == localisationId)
+                .GroupBy(p => p.SocialGroupId)
+                .Select(g => new
+                {
+                    SocialGroupId = g.Key,
+                    PopulationCount = g.Count()
+                })
+                .ToList();
+
+            float totalProduction = 0;
+
+            foreach (var group in populationGroups)
+            {
+                var productionShare = _context.ProductionShares
+                    .Where(ps => ps.SocialGroupId == group.SocialGroupId && ps.ResourceId == resourceId)
+                    .Select(ps => ps.Coefficient)
+                    .FirstOrDefault();
+
+                var resourceAmount = _context.LocalisationResources
+                    .Where(lr => lr.LocationId == localisationId && lr.ResourceId == resourceId)
+                    .Select(lr => lr.Amount)
+                    .FirstOrDefault();
+
+                totalProduction += group.PopulationCount * productionShare * resourceAmount;
+            }
+
+            return totalProduction;
         }
     }
 }
---
+++ Wg-backend-api/DTO/LocalisationDTO.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Wg_backend_api.Models;
+
+namespace Wg_backend_api.DTO
+{
+    public class LocalisationDTO
+    {
+        public int? Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Size { get; set; }
+
+        public int Fortification { get; set; }
+
+        public int NationId { get; set; }
+
+    }
+
+    public class LocalisationResourceDTO
+    {
+        public int? Id { get; set; }
+        public int LocationId { get; set; }
+        public int ResourceId { get; set; }
+        public int Amount { get; set; }
+    }
+
+    public class LocalisationResourceInfoDTO
+    {
+        public string Name { get; set; }
+        public string ResourceName { get; set; }
+        public float Amount { get; set; }
+    }
+
+    public class LocalisationResourceProductionDTO
+    {
+        public string Name { get; set; }
+        public string ResourceName { get; set; }
+        public float Amount { get; set; }
+        public float Production { get; set; }
+    }
+
+    public class LocalisationGeneralInfoDTO
+    {
+        public string Name { get; set; }
+
+        public int Size { get; set; }
+
+        public int Fortification { get; set; }
+
+        public int PopulationSize { get; set; }
+        public float PopulationHappiness { get; set; }
+    }
+
+    public class LocalisationDetailsDTO
+    {
+
+        public string Name { get; set; }
+        public List<LocalisationResourceInfoDTO> Resources { get; set; } = new();
+        public List<LocalisationResourceProductionDTO> ResourceProductions { get; set; } = new();
+        public List<PopulationGroupDTO> PopulationGroups { get; set; }
+
+
+    }
+    
+}
