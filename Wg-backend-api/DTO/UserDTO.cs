19addc72e5ed70f32fdf528de681a323fb54324b
Jakub Śliwka <kubapplll@gmail.com>
2025-06-01 23:49:31 +0200

    feat: set nickname after sso register

---
+++ Wg-backend-api/DTO/UserDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Wg_backend_api.Models;
+
+namespace Wg_backend_api.DTO
+{
+    public class UserDTO
+    {
+    }
+
+    public class UserPathDTO { 
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+
+}
