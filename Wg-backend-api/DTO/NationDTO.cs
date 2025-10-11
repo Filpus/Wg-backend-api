3b5e601b150e6205b28e793b7f484eb3f899cd18
Jakub Śliwka <kubapplll@gmail.com>
2025-10-11 16:46:42 +0200

    Patch nation and fix some warnings

--- Wg-backend-api/DTO/NationDTO.cs
+++ Wg-backend-api/DTO/NationDTO.cs
@@ -32,6 +32,52 @@
 
         public string Color { get; set; }
 
-        public string OwnerName { get; set; }
+        public string? OwnerName { get; set; }
+    }
+
+    public class NationDetailedDTO
+    {
+        public int? Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ReligionDTO Religion { get; set; }
+
+        public CultureDTO Culture { get; set; }
+
+        public string? Flag { get; set; }
+
+        public string Color { get; set; }
+
+        public string? OwnerName { get; set; }
+    }
+
+    public class PostNationDTO
+    {
+        public string Name { get; set; }
+
+        public int Religion { get; set; }
+
+        public int Culture { get; set; }
+
+        public IFormFile? Flag { get; set; }
+
+        public string Color { get; set; }
+    }
+
+    public class PatchNationDTO
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public int? Religion { get; set; }
+
+        public int? Culture { get; set; }
+
+        public IFormFile? Flag { get; set; }
+
+        public string? Color { get; set; }
+
     }
 }
