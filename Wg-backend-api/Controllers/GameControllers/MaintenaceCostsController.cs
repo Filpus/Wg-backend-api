5dc608c51a97f9634396f697b741a137a5a38ea1
Filpus <fil-pus@wp.pl>
2025-10-19 01:08:15 +0200

    Added Costs Menagment.  Unit type controllers already was

--- Wg-backend-api/Controllers/GameControllers/MaintenaceCostsController.cs
+++ Wg-backend-api/Controllers/GameControllers/MaintenaceCostsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Wg_backend_api.Data;
+using Wg_backend_api.DTO;
 using Wg_backend_api.Models;
 using Wg_backend_api.Services;
 
@@ -27,8 +28,71 @@ namespace Wg_backend_api.Controllers.GameControllers
             }
             _context = _gameDbContextFactory.Create(schema);
         }
-        [HttpGet("{id?}")]
 
+
+
+        [HttpGet("unitType/{unitTypeId}")]
+        public async Task<ActionResult<List<UnitTypeResourceInfoDTO>>> GetMaintenaceCostsForUnitType(int unitTypeId)
+        {
+            var list = await _context.MaintenaceCosts
+                .Where(m => m.UnitTypeId == unitTypeId)
+                .Include(m => m.UnitType)
+                .Include(m => m.Resource)
+                .Select(m => new UnitTypeResourceInfoDTO
+                {
+                    Id = (int)m.Id,
+                    UnitTypeId = m.UnitTypeId,
+                    UnitTypeName =  m.UnitType.Name,
+                    ResourceId = m.ResourceId,
+                    ResourceName = m.Resource.Name,
+                    Amount = m.Amount
+                })
+                .ToListAsync();
+
+            return Ok(list);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> UpsertMaintenaceCosts([FromBody] List<UnitTypeResourceDTO> dtos)
+        {
+            if (dtos == null || dtos.Count == 0)
+            {
+                return BadRequest("Brak danych do przetworzenia.");
+            }
+
+            foreach (var dto in dtos)
+            {
+
+
+                MaintenaceCosts entity = null;
+
+                if (dto.Id.HasValue)
+                {
+                    entity = await _context.MaintenaceCosts.FindAsync(dto.Id.Value);
+                }
+
+                if (entity == null)
+                {
+                    entity = new MaintenaceCosts
+                    {
+                        UnitTypeId = dto.UnitTypeId,
+                        ResourceId = dto.ResourceId,
+                        Amount = dto.Amount
+                    };
+                    await _context.MaintenaceCosts.AddAsync(entity);
+                }
+                else
+                {
+                    entity.UnitTypeId = dto.UnitTypeId;
+                    entity.ResourceId = dto.ResourceId;
+                    entity.Amount = dto.Amount;
+                    _context.MaintenaceCosts.Update(entity);
+                }
+            }
+
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
 
         [HttpDelete]
         public async Task<ActionResult> DeleteMaintenaceCosts([FromBody] List<int?> ids)
--- Wg-backend-api/Controllers/GameControllers/ProductionCostsController.cs
+++ Wg-backend-api/Controllers/GameControllers/ProductionCostsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Wg_backend_api.Data;
+using Wg_backend_api.DTO;
 using Wg_backend_api.Models;
 using Wg_backend_api.Services;
 
@@ -49,6 +50,69 @@ namespace Wg_backend_api.Controllers.GameControllers
             _context.ProductionCosts.RemoveRange(productionCosts);
             await _context.SaveChangesAsync();
 
+            return Ok();
+        }
+
+        [HttpGet("unitType/{unitTypeId}")]
+        public async Task<ActionResult<List<UnitTypeResourceInfoDTO>>> GeProductionCostsForUnitType(int unitTypeId)
+        {
+            var list = await _context.ProductionCosts
+                .Where(m => m.UnitTypeId == unitTypeId)
+                .Include(m => m.UnitType)
+                .Include(m => m.Resource)
+                .Select(m => new UnitTypeResourceInfoDTO
+                {
+                    Id = (int)m.Id,
+                    UnitTypeId = m.UnitTypeId,
+                    UnitTypeName = m.UnitType.Name,
+                    ResourceId = m.ResourceId,
+                    ResourceName = m.Resource.Name,
+                    Amount = m.Amount
+                })
+                .ToListAsync();
+
+            return Ok(list);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> UpsertProductionCosts([FromBody] List<UnitTypeResourceDTO> dtos)
+        {
+            if (dtos == null || dtos.Count == 0)
+            {
+                return BadRequest("Brak danych do przetworzenia.");
+            }
+
+            foreach (var dto in dtos)
+            {
+
+
+                ProductionCost entity = null;
+
+                if (dto.Id.HasValue)
+                {
+                    entity = await _context.ProductionCosts.FindAsync(dto.Id.Value);
+                }
+
+                if (entity == null)
+                {
+                    entity = new ProductionCost
+                    {
+                        UnitTypeId = dto.UnitTypeId,
+                        ResourceId = dto.ResourceId,
+                        Amount = dto.Amount
+                    };
+                    await _context.ProductionCosts.AddAsync(entity);
+                }
+                else
+                {
+                    entity.UnitTypeId = dto.UnitTypeId;
+                    entity.ResourceId = dto.ResourceId;
+                    entity.Amount = dto.Amount;
+                    _context.ProductionCosts.Update(entity);
+                }
+            }
+
+            await _context.SaveChangesAsync();
             return Ok();
         }
     }
