12ec5ee62ff5ed238d462521e15121fb2de8cf6f
Jakub Śliwka <kubapplll@gmail.com>
2025-10-22 00:16:49 +0200

    FEAT: Map managment and fix some endpoint

--- Wg-backend-api/Controllers/GameControllers/MapAccessController.cs
+++ Wg-backend-api/Controllers/GameControllers/MapAccessController.cs
@@ -19,115 +19,139 @@ namespace Wg_backend_api.Controllers.GameControllers
 
         public MapAccessController(IGameDbContextFactory gameDbFactory, ISessionDataService sessionDataService)
         {
-            _gameDbContextFactory = gameDbFactory;
-            _sessionDataService = sessionDataService;
+            this._gameDbContextFactory = gameDbFactory;
+            this._sessionDataService = sessionDataService;
 
-            string schema = _sessionDataService.GetSchema();
+            string schema = this._sessionDataService.GetSchema();
             if (string.IsNullOrEmpty(schema))
             {
                 throw new InvalidOperationException("Brak schematu w sesji.");
             }
 
-            _context = _gameDbContextFactory.Create(schema);
-            string nationIdStr = _sessionDataService.GetNation();
-            _nationId = string.IsNullOrEmpty(nationIdStr) ? null : int.Parse(nationIdStr);
-        }
-
-        [HttpGet]
-        public async Task<List<(int, int)>> GetMapAccesses()
-        {
-            var mapAccesses = await this._context.MapAccesses
-                .Select(x => new ValueTuple<int, int>(x.NationId, x.MapId))
-                .ToListAsync();
-
-            return mapAccesses;
+            this._context = this._gameDbContextFactory.Create(schema);
+            string nationIdStr = this._sessionDataService.GetNation();
+            this._nationId = string.IsNullOrEmpty(nationIdStr) ? null : int.Parse(nationIdStr);
         }
 
         [HttpGet("{mapId?}")]
         public async Task<ActionResult<List<MapAccessInfoDTO>>> GetAllMapAccesses(int? mapId)
         {
+            var mapAccesses = new List<MapAccessInfoDTO>();
             if (mapId.HasValue)
             {
-                var mapAccesses = await _context.MapAccesses
+                mapAccesses = await this._context.MapAccesses
                    .Include(ma => ma.Map)
                    .Include(ma => ma.Nation)
                    .Where(ma => ma.MapId == mapId)
+                    .Select(ma => new MapAccessInfoDTO
+                    {
+                        NationId = ma.NationId,
+                        MapId = ma.MapId,
+                        NationName = ma.Nation.Name,
+                        NationImage = ma.Nation != null ? ma.Nation.Flag ?? string.Empty : string.Empty,
+                        MapName = ma.Map.Name,
+                    })
                    .ToListAsync();
             }
             else
             {
-                var mapAccesses = await _context.MapAccesses
+                mapAccesses = await this._context.MapAccesses
                    .Include(ma => ma.Map)
                    .Include(ma => ma.Nation)
+                    .Select(ma => new MapAccessInfoDTO
+                    {
+                        NationId = ma.NationId,
+                        MapId = ma.MapId,
+                        NationName = ma.Nation.Name,
+                        NationImage = ma.Nation != null ? ma.Nation.Flag ?? string.Empty : string.Empty,
+                        MapName = ma.Map.Name,
+                    })
                    .ToListAsync();
             }
 
-            var result = await _context.MapAccesses
-                .Select(ma => new MapAccessInfoDTO
-                {
-                    NationId = ma.NationId,
-                    MapId = ma.MapId,
-                    NationName = ma.Nation.Name,
-                    NationImage = ma.Nation != null ? ma.Nation.Flag ?? string.Empty : string.Empty,
-                    MapName = ma.Map.Name,
-                })
-                .ToListAsync();
-
-            return Ok(result);
+            return this.Ok(mapAccesses);
         }
 
         [HttpPost]
-        public async Task<ActionResult> PostMapAccesses([FromBody] int nationId, int mapId)
+        public async Task<ActionResult> PostMapAccesses([FromBody] List<MapAccessCreateDTO> ids)
         {
-            if (nationId <= 0 || mapId <= 0)
+            if (ids == null || !ids.Any())
             {
-                return this.BadRequest("Inappropriate ID");
+                return this.BadRequest("No map access entries provided.");
             }
 
-            var existingMapAccess = await this._context.MapAccesses
-                .FirstOrDefaultAsync(ma => ma.NationId == nationId && ma.MapId == mapId);
-
-            if (existingMapAccess == null)
-                return this.BadRequest("Map access already exists");
-
-            var map = await this._context.Maps.FindAsync(mapId);
-            if (map == null)
-                return this.BadRequest("Map does not exist");
-
-            var nation = await this._context.Nations.FindAsync(nationId);
-            if (nation == null)
-                return this.BadRequest("Nation does not exist");
-
-            var newMapAccess = new MapAccess
+            var newMapAccesses = new List<MapAccess>();
+            foreach (var mapaccess in ids)
             {
-                NationId = nationId,
-                MapId = mapId,
-            };
+                int nationId = mapaccess.NationId;
+                int mapId = mapaccess.MapId;
+                if (nationId < 0 || mapId < 0)
+                {
+                    return this.BadRequest("Inappropriate ID");
+                }
 
-            this._context.MapAccesses.Add(newMapAccess);
+                var existingMapAccess = await this._context.MapAccesses
+                    .FirstOrDefaultAsync(ma => ma.NationId == nationId && ma.MapId == mapId);
+                Console.WriteLine(existingMapAccess);
+                if (existingMapAccess != null)
+                {
+                    return this.BadRequest("Map access already exists");
+                }
+
+                var map = await this._context.Maps.FirstOrDefaultAsync(map => map.Id == mapId);
+                if (map == null)
+                {
+                    return this.BadRequest("Map does not exist");
+                }
+
+                var nation = await this._context.Nations.FirstOrDefaultAsync(map => map.Id == nationId);
+                if (nation == null)
+                {
+                    return this.BadRequest("Nation does not exist");
+                }
+
+                var newMapAccess = new MapAccess
+                {
+                    NationId = nationId,
+                    MapId = mapId,
+                };
+
+                newMapAccesses.Add(newMapAccess);
+                this._context.MapAccesses.Add(newMapAccess);
+            }
+
             await this._context.SaveChangesAsync();
 
-            return this.CreatedAtAction(nameof(this.GetMapAccesses), newMapAccess);
+            return this.CreatedAtAction(nameof(this.GetAllMapAccesses), new {});
         }
 
         [HttpDelete]
-        public async Task<ActionResult> DeleteMapAccesses([FromBody] List<(int nationId, int mapId)> ids)
+        public async Task<ActionResult> DeleteMapAccesses([FromBody] List<MapAccessCreateDTO> ids)
         {
             if (ids == null || ids.Count == 0)
             {
                 return this.BadRequest("Brak ID do usunięcia.");
             }
 
-            var mapAccesses = await this._context.MapAccesses
-                .Where(ma => ids.Any(id => id.nationId == ma.NationId && id.mapId == ma.MapId))
-                .ToListAsync();
-
-            if (mapAccesses.Count == 0)
+            foreach (var item in ids)
             {
-                return this.NotFound("Nie znaleziono dostępu do map do usunięcia.");
+                if (item.NationId < 0 || item.MapId < 0)
+                {
+                    return this.BadRequest("Nieprawidłowe ID.");
+                }
             }
 
-            this._context.MapAccesses.RemoveRange(mapAccesses);
+            foreach (var id in ids)
+            {
+                var mapAccess = await this._context.MapAccesses
+                    .FirstOrDefaultAsync(ma => ma.NationId == id.NationId && ma.MapId == id.MapId);
+
+                if (mapAccess != null)
+                {
+                    this._context.MapAccesses.Remove(mapAccess);
+                }
+            }
+
             await this._context.SaveChangesAsync();
 
             return this.Ok();
